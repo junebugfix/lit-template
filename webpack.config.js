ead8dc0bfe94230c649f5c3a5562bb72992b1874
Hank Hester <hankhester1@gmail.com>
2019-11-10 10:58:22 -0800

    Add sass support and hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: ':)'
+      title: 'The Luminous Pariah'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: dev
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
