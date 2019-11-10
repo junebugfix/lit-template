a35427d1afa82f3869bc189fc6f427858470614d
Hank Hester <hankhester1@gmail.com>
2019-11-10 02:32:28 -0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { html, render } from 'lit-html';
+
+const home = html`
+  <div>Hey</div>
+`;
+
+render(home, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: ':)'
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+};
